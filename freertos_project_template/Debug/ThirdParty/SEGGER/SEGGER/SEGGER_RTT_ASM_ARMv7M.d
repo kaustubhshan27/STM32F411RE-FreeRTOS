e81e3c17857054b9c13247ac6ecac10cb93216be
kaustubhshan27 <kaustubhshan@gmail.com>
2023-07-11 11:05:38 +0100

    removed debug files from all projects

--- freertos_project_template/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/MAHE/STM32CubeIDE/rtos_workspace/freertos_project_template/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-C:/Users/MAHE/STM32CubeIDE/rtos_workspace/freertos_project_template/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
--- freertos_project_template/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- C:/Users/MAHE/STM32CubeIDE/rtos_workspace/freertos_project_template/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/MAHE/STM32CubeIDE/rtos_workspace/freertos_project_template/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-
-C:/Users/MAHE/STM32CubeIDE/rtos_workspace/freertos_project_template/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-C:/Users/MAHE/STM32CubeIDE/rtos_workspace/freertos_project_template/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
